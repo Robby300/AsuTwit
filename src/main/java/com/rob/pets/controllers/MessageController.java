fa02d68b0e7f1c4e09b79da52d75a5b3a4d4679c
Robert Imangulov <robby300im@gmail.com>
2021-12-19 22:09:42 +0500

    rest controller

--- src/main/java/com/rob/pets/controllers/MessageController.java
+++ src/main/java/com/rob/pets/controllers/MessageController.java
@@ -1,7 +1,14 @@
 package com.rob.pets.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("message")
 public class MessageController {
+    @GetMapping
+    public String list() {
+        return "index";
+    }
 }
