fecae5f94147a856df2dcc7935a5e6f4dee36256
Robert Imangulov <robby300im@gmail.com>
2021-12-21 22:14:43 +0500

    main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,8 @@
+// Определяем новый компонент под именем todo-item
+Vue.component('todo-item', {
+    template: '<li>Это одна задача в списке</li>'
+})
+
 var app = new Vue({
     el: '#app',
     data: {
