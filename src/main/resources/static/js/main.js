a6696a2056fa3b6469506e6f7142ce4f8eb3c740
Engineer <rob-prik@mail.ru>
2021-12-22 09:50:18 +0500

    we need more JS!

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,55 @@
-// Определяем новый компонент под именем todo-item
-Vue.component('todo-item', {
-    template: '<li>Это одна задача в списке</li>'
-})
+var messageApi = Vue.resource('/message{/id}');
+
+Vue.component('message-form', {
+    props: ['messages'],
+    data: function () {
+        return{
+            text: ''
+        }
+    },
+    template:
+        '<div>' +
+            '<input type="text" placeholder="Write your message" v-model = "text"/>' +
+            '<input type="button" value="Save" v-on:click="save"/>' +
+        '</div>',
+    methods: {
+        save: function () {
+            var message = { text: this.text};
+            messageApi.save({}, message).then(result =>
+                result.json().then(data => {
+                    this.messages.push(data);
+                    this.text = '';
+                })
+            )
+        }
+    }
+});
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><i>({{ message.id }})</i> {{ message.text}}</div>'
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template:
+        '<div>' +
+        '<message-form :messages= "messages" />' +
+        '<message-row v-for="message in messages" :key = "message.id" :message = "message"/>' +
+        '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
+});
 
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages" />',
     data: {
-        message: 'Привет, Vue!'
+        messages: []
     }
-})
+});
