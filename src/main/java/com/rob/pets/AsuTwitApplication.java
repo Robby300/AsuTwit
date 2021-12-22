7d8012071fcae53bd7f238e17660df1a76a5a0ca
Engineer <rob-prik@mail.ru>
2021-12-22 14:43:22 +0500

    Refactoring ro AsuTwit

--- src/main/java/com/rob/pets/AsuTwitApplication.java
+++ src/main/java/com/rob/pets/AsuTwitApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetsApplication {
+public class AsuTwitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetsApplication.class, args);
+		SpringApplication.run(AsuTwitApplication.class, args);
 	}
 
 }
