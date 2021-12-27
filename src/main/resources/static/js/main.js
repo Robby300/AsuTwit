e2104d63995658d678ba005d7038683a7269a3ba
Engineer <rob-prik@mail.ru>
2021-12-27 18:06:14 +0500

    added WebSecurityConfig and Oauth2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -104,7 +104,7 @@ var app = new Vue({
         '<div>' +
         '<div v-if="!profile">Необходимо авторизоваться через <a href="/login">Google</a></div>' +
         '<div v-else>' +
-        '<div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>' +
+        '<div>{{profile.name}}&nbsp; <a href="/logout">Выйти</a></div>' +
         '<messages-list :messages="messages" />' +
         '</div>' +
         '</div>',
